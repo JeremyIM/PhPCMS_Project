16e2da1dd0777a1ac69973702a710fe0600ddb93
JeremyIM <jazzamoo@gmail.com>
2014-11-24 10:24:53 -0800

    This is pretty much  ateset

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: inet2005
- * Date: 11/24/14
- * Time: 10:14 AM
- */ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page title goes here!</title>
+    </head>
+    <body>
+        <?php
+            echo "Why does this not want to work?";
+        ?>
+    </body>
+</html>
