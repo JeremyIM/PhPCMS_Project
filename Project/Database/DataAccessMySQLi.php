dc93f7354d8c2340a46081ca296d440853d29953
blairlanderson <blair.l.anderson@gmail.com>
2014-11-26 15:11:40 -0800

    Added Retrieves for Business Classes
    Cleaned up some naming irregularities

--- Project/Database/DataAccessMySQLi.php
+++ Project/Database/DataAccessMySQLi.php
@@ -33,6 +33,8 @@ class DataAccessMySQLi extends dataAccess
             die('Could not retrieve pages from the Database: ' .
                 $this->dbConnection->error);
         }
+        return $this->result->fetch_array();
+
     }//end getPages
 
     public function getArticles()
@@ -43,11 +45,12 @@ class DataAccessMySQLi extends dataAccess
             die('Could not retrieve pages from the Database: ' .
                 $this->dbConnection->error);
         }
+        return $this->result->fetch_array();
 
     }//end getArticle
 
 
-    public function getContent()
+    public function getContentArea()
     {
         $this->result =@$this->dbConnection->query("SELECT * FROM content_area");
         if(!$this->result)
@@ -55,9 +58,123 @@ class DataAccessMySQLi extends dataAccess
             die('Could not retrieve pages from the Database: ' .
                 $this->dbConnection->error);
         }
+        return $this->result->fetch_array();
 
     }//end getContent
 
+    public function getCss()
+    {
+        $this->result =@$this->dbConnection->query("SELECT * FROM css");
+        if(!$this->result)
+        {
+            die('Could not retrieve pages from the Database: ' .
+                $this->dbConnection->error);
+        }
+        return $this->result->fetch_array();
+
+    }//end getContent
+
+     //fetches
+    public function fetchCssID($row)
+    {
+        return $row['css_id'];
+    }
+    public function fetchCssName($row)
+    {
+        return $row['name'];
+    }
+    public function fetchCssDescription($row)
+    {
+        return $row['description'];
+    }
+    public function fetchCssActiveStatus($row)
+    {
+        return $row['active_status'];
+    }
+    public function fetchCssStyleSnippet($row)
+    {
+        return $row['style_snippet'];
+    }
+
+    public function fetchPagePageId($row)
+    {
+        return $row['page_id'];
+    }
+    public function fetchPageName($row)
+    {
+        return $row['page_name'];
+    }
+    public function fetchPageWebName($row)
+    {
+        return $row['web_name'];
+    }
+    public function fetchPageDescription($row)
+    {
+        return $row['description'];
+    }
+    public function fetchPageActiveCss($row)
+    {
+        return $row['active_css'];//article fetches
+    }
+        public function fetchArticleID($row)
+    {
+        return $row['article_id'];
+    }
+        public function fetchArticleName($row)
+    {
+        return $row['name'];
+    }
+        public function fetchArticleTitle($row)
+    {
+        return $row['title'];
+    }
+        public function fetchArticleDesc($row)
+    {
+        return $row['description'];
+    }
+        public function fetchArticleAllPages($row)
+    {
+        return $row['all_pages'];
+    }
+        public function fetchArticlePage($row)
+    {
+        return $row['page_id'];
+    }
+        public function fetchArticleContentArea($row)
+    {
+        return $row['content_area_id'];
+    }
+        public function fetchArticleContent($row)
+    {
+        return $row['the_content'];
+    }
+
+        //Content area fetches
+        public function fetchContentAreaId($row)
+    {
+        return $row['content_id'];
+    }
+        public function fetchContentAreaName($row)
+    {
+        return $row['name'];
+    }
+        public function fetchContentAreaDivName($row)
+    {
+        return $row['div_name'];
+    }
+        public function fetchContentAreaPageOrder($row)
+    {
+        return $row['page_order_pos'];
+    }
+        public function fetchContentAreaDesc($row)
+    {
+        return $row['description'];
+    }
+
+
+
+
+    //add'l selects
     public function getSinglePage($pageIDin)
     {
         $this->result =@$this->dbConnection->query("SELECT * FROM mydb.page WHERE page_id='$pageIDin'");
@@ -77,8 +194,6 @@ class DataAccessMySQLi extends dataAccess
                 $this->dbConnection->error);
         }
     }
-
-
 
 
 }//end class DataAccessMySQLi
--- Project/Database/dbConn.php
+++ Project/Database/dbConn.php
@@ -25,10 +25,56 @@ abstract class dataAccess
 
     public abstract function getArticles();
 
-    public abstract function getContent();
+    public abstract function getContentArea();
+
+    public abstract function getCss();
 
     public abstract function getSinglePage($pageIDin);
 
     public abstract function getAllArticle($pageIDin,$contentIDin);
+
+    //CSS fetches
+    public abstract function fetchCssID($row);
+
+    public abstract function fetchCssName($row);
+
+    public abstract function fetchCssDescription($row);
+
+    public abstract function fetchCssActiveStatus($row);
+
+    public abstract function fetchCssStyleSnippet($row);
+
+    //Page Fetches
+    public abstract function fetchPagePageId($row);
+
+    public abstract function fetchPageName($row);
+
+    public abstract function fetchPageWebName($row);
+
+    public abstract function fetchPageDescription($row);
+
+    public abstract function fetchPageActiveCss($row);
+
+    //article fetches
+    public abstract function fetchArticleID($row);
+    public abstract function fetchArticleName($row);
+    public abstract function fetchArticleTitle($row);
+    public abstract function fetchArticleDesc($row);
+    public abstract function fetchArticleAllPages($row);
+    public abstract function fetchArticlePage($row);
+    public abstract function fetchArticleContentArea($row);
+    public abstract function fetchArticleContent($row);
+
+    //Content area fetches
+    public abstract function fetchContentAreaId($row);
+    public abstract function fetchContentAreaName($row);
+    public abstract function fetchContentAreaDivName($row);
+    public abstract function fetchContentAreaPageOrder($row);
+    public abstract function fetchContentAreaDesc($row);
+
+
+
+
+
 
 }//end dataAccess class
