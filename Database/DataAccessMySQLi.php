8468659b8cc7f59880a31b9e96d1906322f32a01
JeremyIM <jazzamoo@gmail.com>
2014-11-26 13:50:03 -0800

    added buttloads of fetches

--- Database/DataAccessMySQLi.php
+++ Database/DataAccessMySQLi.php
@@ -79,6 +79,55 @@ class DataAccessMySQLi extends dataAccess
     }
 
 
+    //fetches
+
+    public function fetchCssID($row)
+    {
+        return $row['css_id'];
+    }
+    public function fetchCssName($row)
+    {
+        return $row['name'];
+    }
+    public function fetchCssDescription($row)
+    {
+        return $row['description'];
+    }
+    public function fetchCssActiveStatus($row)
+    {
+        return $row['active_status'];
+    }
+    public function fetchCssStyleSnippet($row)
+    {
+        return $row['style_snippet'];
+    }
+
+    public function fetchPagePageId($row)
+    {
+        return $row['page_id'];
+    }
+    public function fetchPageName($row)
+    {
+        return $row['page_name'];
+    }
+    public function fetchPageWebName($row)
+    {
+        return $row['web_name'];
+    }
+    public function fetchPageDescription($row)
+    {
+        return $row['description'];
+    }
+    public function fetchPageActiveCss($row)
+    {
+        return $row['active_css'];
+    }
+
+
+
+
+
+
 
 
 }//end class DataAccessMySQLi
